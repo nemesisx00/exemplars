e78c2277a5aa950662392a9d8cdeac796f1fa112
Peter Lunneberg <plunn.dev@gmail.com>
2017-10-29 12:37:52 -0400

    Updated .gitignore for VS C# projects. Added C# Hello World.

---
+++ c#/helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        const string output = "Hello World!";
+
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine(output);
+        }
+    }
+}
