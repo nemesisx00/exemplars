ae4d0393e2745e83125e8bf07eb19081b939ed0d
Peter Lunneberg <plunn.dev@gmail.com>
2017-10-29 12:13:01 -0400

    Added Java hello world

---
+++ java/src/plunn/exemplars/HelloWorld.java
@@ -0,0 +1,11 @@
+package plunn.exemplars;
+
+public class HelloWorld
+{
+	public static final String output = "Hello World!";
+	
+	public static void main(String[] args)
+	{
+		System.out.println(output);
+	}
+}
